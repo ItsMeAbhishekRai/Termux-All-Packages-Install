474d1abfd8d983c246e0dac144a2b4483cdab87a
BLUE HAT HACKER <abhishekraiar0@gmail.com>
2019-05-05 11:59:43 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,82 @@
+#!/data/data/com.termux/files/usr/bash
+echo $red 
+ echo ""----------------------------------------------------------------------------- ""
+echo " This Script is Made by Blue Hat Hacker"
+echo "---------------------------------------------------------------------------------"
+
+echo
+
+echo  "#####################################"
+echo   "  http://www.bluehathacker.ga     "               
+ echo   " Learn Eathical Hacking Free"                                                                    
+echo   "####################################"
+
+echo
+echo
+echo
+echo
+
+echo "."
+
+apt update -y                      #update in progress
+apt install python -y           #installation......
+apt install python2 -y
+apt install ruby -y 
+apt install git -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE  MY YOUTUBE CHANEL     "               
+ echo   " https://www.youtube.com/c/bluehathacker "                                                                    
+echo   "####################################"
+
+apt install php -y 
+apt install perl -y
+apt install nmap -y
+
+echo  "#####################################"
+echo   "  GO TO MY WEBSITE AND LEARN HACKING  "             
+ echo   " http://www.bluehathacker.ga "                                                                    
+echo   "####################################"
+
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install macchanger -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE    BLUE HAT HACKER YOUTUBE CHANEL "               
+ echo   " EATHICAL HACKING VIDEOS "                                                                    
+echo   "####################################"
+
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+
+echo  "#####################################"
+echo   " MY WORDPRESS BLOG     "               
+ echo   " https://www.bluehathackergroup.wordpress.com "                                                                    
+echo   "####################################"
+
+apt install unzip -y
+apt install tor -y
+apt install wget -y
+
+echo  "#####################################"
+echo   "  https://www.YouTube.com/c/bluehathacker      "               
+ echo   " EATHICAL HACKING VIDEOS "                                                                    
+echo   "####################################"
+
+apt install wcalc -y 
+apt install openssl
+apt install bmon -y
+
+echo  "#####################################"
+echo   "  GO TO MY WEBSITE      "               
+ echo   " http//www.bluehathacker.ga"                                                                    
+echo   "####################################"
+
+echo " If You Connected To Internet Then  "
+echo " YOUR PACKAGES IS INSTALLED SUCCESSFULLY "
+
+exit
